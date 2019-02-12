56fca41e5928e523d951be43a41cd329ab699a6c
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-02-12 20:07:52 +0530

    2_2.c

---
+++ 2_2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,a[100],i,temp,j;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<k;i++)
+    {
+        temp=a[n-1];
+            for(j=n-1;j>=0;j--)
+            {
+                a[j+1]=a[j];
+            }
+        a[0]=temp;
+    }
+    for(i=0;i<n;i++)
+    printf("%d ",a[i]);
+    
+}
